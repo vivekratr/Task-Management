72676c50af8b4547836d95d318e4189dade78230
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 <vivekratr@gmail.com>
2023-10-15 20:40:33 +0530

    Updated room code function

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -52,6 +52,7 @@ function ListItems() {
 
   const handleNext =async () => {
     const roomCode ={code: generateUniqueRoomCode(6)};
+    console.log(roomCode);
     await axios({
       method: 'post',
       url: 'http://localhost:3001/roomVerify',
