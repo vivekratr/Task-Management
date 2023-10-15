c04918c1b35b6d8a1798377eabd9a2cd97b49311
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 <vivekratr@gmail.com>
2023-10-15 20:01:04 +0530

    a

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,13 +2,8 @@ import React from "react";
 
 function Header(){
     return (
-<<<<<<< HEAD
     <div  className="header">
         <img src="https://cdn.discordapp.com/attachments/1147548909296689282/1147549038724522056/image.png" alt="App-logo" />
-=======
-    <div className="header">
-        <img src= "https://cdn.discordapp.com/attachments/1147548909296689282/1147549038724522056/image.png" alt="App-logo" />
->>>>>>> 14e04e737c2307f5ad4c3d2b202e59f9cddde6e6
         <h1>Taskify</h1>
     </div>
     )
